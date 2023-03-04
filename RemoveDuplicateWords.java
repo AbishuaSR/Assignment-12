63caca60932a8761b3d96ca48e468172c4ab675d
AbishuaSR <123636307+AbishuaSR@users.noreply.github.com>
2023-03-04 21:34:46 +0530

    Add files via upload

---
+++ RemoveDuplicateWords.java
@@ -0,0 +1,19 @@
+package week5.day5;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class RemoveDuplicateWords {
+	public static void main(String[] args) {
+
+		String text = "We learn java basics as part of java sessions in java week1";
+		String[] split=text.split(" ");
+		Set<String> words=new TreeSet<String>();
+		for(int i=0;i<split.length;i++) {
+			words.add(split[i]);
+		}
+		System.out.println(words);
+
+	}
+
+}
